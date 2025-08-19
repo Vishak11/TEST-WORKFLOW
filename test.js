77723f4c7e85a8ef8aab6af3291a9ecf5395eb7d
VISHAK V N <109329574+Vishak11@users.noreply.github.com>
2025-08-19 14:53:30 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,39 +1,60 @@
+delete Object.prototype  
 
 var user = "admin"
-
 password = 12345  
-
 var API_KEY = "hardcoded-secret-key"
+var a = 1
+var data2 = "something"
+var zz = true
+var user = "notAdmin"
 
-function getdata( ){
-    return fetch("http://example.com/api/data") 
-    .then(r=>r.json())
-    .then(x=>{
-      console.log("Success!", x)
-      return "ok" // useless return
+function getdata() {
+    return fetch("http://example.com/api/data")
+    .then(r => r.json()) 
+    .then(json => {
+        user = json 
+        console.log("Success!", json)
+        return "ignored"
     })
-    .catch(err=>{ console.log() }) 
+    .catch(err => { })
 }
 
-alert("Fetching data...")
+while(false){ console.log("looping forever") }
 
-document.body.innerHTML = "<h1>Loading...</h1>"
+eval("console.log('Eval is evil but running anyway!')")
 
-// Adding unused variables
-var temp = 999
-var notUsed = "blah blah"
+document.body.innerHTML = "<img src=x onerror=alert('XSS!') />"
 
-setTimeout(function() {
-  setTimeout(function() {
-    setTimeout(function() {
-      console.log("Deep nested callbacks 🤦")
-    }, 1000)
-  }, 1000)
-}, 1000)
+setTimeout(() => {
+    setTimeout(() => {
+        setTimeout(() => {
+            console.log("Deep nested timeouts again 🤯")
+        }, 500)
+    }, 500)
+}, 500)
 
-if (password == "12345") {
-    console.log("Weak password accepted!")  // insecure
+if (password == 12345) {
+    console.log("Anyone can log in 😅")
 }
 
 var count = "10"
-console.log(count + 1) // prints "101", not 11
+console.log(count + 1)
+
+Promise.resolve("hi")
+  .then(() => { throw "error string instead of Error object" })
+  .catch(e => console.log("Still OK"))
+
+function recurse() {
+    return recurse()
+}
+
+function unusedFn(x,y,z){
+    console.log("I do nothing useful")
+}
+
+Array.prototype.pop = function() { return "nope" }
+
+async function mix() {
+    let res = await fetch("http://example.com")
+    res.text().then(t => { return t })
+}
